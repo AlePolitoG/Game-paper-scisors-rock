4da3c12008379ebae7a522aa5b52e525e2bbac1e
AlePolitoG <polto1010@gmail.com>
2023-07-17 15:07:34 -0500

    Create new algorithms
    do Doc, and algorithm of the game

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
-let random = Math.round(Math.random() * 10);
-
 function computer() {
-  const result = random < 3 ? "paper" : random < 6 ? "scissors" : "rock";
+  let random = Math.round(Math.random() * 10);
+  const result = random < 3 ? "paper" : random < 6 ? "scissor" : "rock";
   return result;
 }
+const paragraph = document.querySelector("p");
+const btPaper = document.getElementById("paper");
+const btRock = document.querySelector("#rock");
+const btScissor = document.querySelector("#scissors");
+const tryText = document.getElementById("winner-loser");
+const scoreH = document.getElementById("playerScore");
+const scoreC = document.getElementById("computerScore");
 
-function human() {
-  const result = prompt("Rock, paper, o scissor?");
-  return result.toLowerCase();
+tryText.innerText = "TRY!";
+
+scoreH.innerText = 0;
+scoreC.innerText = 0;
+
+btPaper.onclick = () => {
+  casePaper();
+};
+
+function casePaper() {
+  if (computer() === "paper") {
+    tryText.innerText = "Tie!";
+  } else if (computer() === "rock") {
+    tryText.innerText = "Good!";
+    scoreH.innerText++;
+  } else if (computer() === "scissor") {
+    tryText.innerText = ":(";
+    scoreC.innerText++;
+  }
+  if (scoreH.innerText == 5 || scoreC.innerText == 5) {
+    btPaper.onclick = null;
+    btRock.onclick = null;
+    btScissor.onclick = null;
+  }
 }
-var computer_score = 0;
-var human_score = 0;
-function game() {
-  if (human() === computer()) {
-    return human_score + 0;
-  } else if (
-    (human() === "rock" && computer() === "scissor") ||
-    (human() === "paper" && computer() === "rock") ||
-    (human() === "scissor" && computer() === "paper")
-  ) {
-    () => human_score++;
-  } else if (
-    (human() === "rock" && computer() === "paper") ||
-    (human() === "paper" && computer() === "scissor") ||
-    (human() === "scissor" && computer() === "rock")
-  ) {
-    () => computer_score++;
+
+btRock.onclick = () => {
+  caseRock();
+};
+
+function caseRock() {
+  if (computer() === "rock") {
+    tryText.innerText = "Tie!";
+  } else if (computer() === "scissor") {
+    tryText.innerText = "Good!";
+    scoreH.innerText++;
+  } else if (computer() === "paper") {
+    tryText.innerText = ":(";
+    scoreC.innerText++;
+  }
+  if (scoreH.innerText == 5 || scoreC.innerText == 5) {
+    btPaper.onclick = null;
+    btRock.onclick = null;
+    btScissor.onclick = null;
+  }
+}
+
+btScissor.onclick = () => {
+  caseScissor();
+};
+
+function caseScissor() {
+  if (computer() === "scissor") {
+    tryText.innerText = "Tie!";
+  } else if (computer() === "paper") {
+    tryText.innerText = "Good!";
+    scoreH.innerText++;
+  } else if (computer() === "rock") {
+    tryText.innerText = ":(";
+    scoreC.innerText++;
+  }
+  if (scoreH.innerText == 5 || scoreC.innerText == 5) {
+    btPaper.onclick = null;
+    btRock.onclick = null;
+    btScissor.onclick = null;
   }
 }
